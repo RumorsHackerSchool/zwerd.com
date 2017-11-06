e02a30e028ab09694611cbf26032e293f7638ff2
guy.zwerdling <guy.zwerdling@gmail.com>
2017-11-06 08:11:01 +0200

    update

--- ZWERDv3/scripts/onload.js
+++ ZWERDv3/scripts/onload.js
@@ -1,3 +1,12 @@
-let fs = require('../blogs/Prophet-Riddles-App-update.html');
-console.log(fs);
-fs.readFile(file, [encoding], [callback]);
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "Prophet-Riddles-App-update.html", true);
+  xhttp.send();
+}
+window.addEventListener("load",loadDoc);
---
+++ techy/index.md
@@ -0,0 +1,7 @@
+<% set('username', 'Big Joe') %>
+
+# Article title
+
+> author: <% get('username') %>
+
+Hello, my name is <% get('username') %>. I'm a web developer.
